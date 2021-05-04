9c70efb0571028515a2e25bff2e2140ef1cc5bc5
Robert Haworth <rhaworth@acculynx.com>
2021-05-04 16:21:05 -0500

    Commit
    Changes

--- RestaurantReviewer/view models/CreateReviewViewModel.swift
+++ RestaurantReviewer/view models/CreateReviewViewModel.swift
@@ -5,4 +5,39 @@
 //  Created by Robert Haworth on 5/3/21.
 //
 
-import Foundation
+import SwiftUI
+import CoreData
+import Combine
+
+class CreateReviewViewModel: ObservableObject {
+    @Published var visitDate: Date = Date()
+    @Published var reviewRating: Int = 1
+    @Published var reviewText: String = ""
+    
+    private let restaurant: Restaurant
+    
+    @Published var error = false
+    
+    init(for restaurant: Restaurant) {
+        self.restaurant = restaurant
+    }
+    
+    func save(with context: NSManagedObjectContext) -> Bool {
+        let newReview = Review(context: context)
+        newReview.id = UUID()
+        newReview.visitDate = visitDate
+        newReview.createdDate = Date()
+        newReview.notes = reviewText
+        newReview.starCount = Int16(reviewRating)
+        newReview.restaurant = restaurant
+        
+        do {
+            try context.save()
+            return true
+        } catch {
+            print("deal with errors here \(error)")
+            self.error = true
+            return false
+        }
+    }
+}
--- RestaurantReviewer/view models/RestaurantDetailViewModel.swift
+++ RestaurantReviewer/view models/RestaurantDetailViewModel.swift
@@ -5,4 +5,46 @@
 //  Created by Robert Haworth on 5/3/21.
 //
 
+import SwiftUI
 import Foundation
+import CoreData
+
+class RestaurantDetailViewModel: NSObject, ObservableObject {
+    
+    @Published var reviews: [Review] = []
+    
+    private let resultsController: NSFetchedResultsController<Review>
+    
+    let restaurant: Restaurant
+    
+    init(context: NSManagedObjectContext, restaurant: Restaurant) {
+        self.restaurant = restaurant
+        
+        let request = NSFetchRequest<Review>(entityName: "Review")
+        request.predicate = NSPredicate(format: "restaurant == %@", restaurant)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \Review.createdDate, ascending: true)]
+        
+        resultsController = NSFetchedResultsController(fetchRequest: request,
+                                                       managedObjectContext: context,
+                                                       sectionNameKeyPath: nil,
+                                                       cacheName: nil)
+        
+        super.init()
+        
+        resultsController.delegate = self
+        
+        do {
+            try resultsController.performFetch()
+            reviews = resultsController.fetchedObjects ?? []
+        } catch {
+            print("big problem")
+        }
+    }
+}
+
+extension RestaurantDetailViewModel: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard let items = controller.fetchedObjects as? [Review] else { return }
+        reviews = items
+    }
+}
